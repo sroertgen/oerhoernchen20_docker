4f6b761facdb782af74fa616a48a388113a420cc
@s.roertgen <sroertgen@gmail.com>
2019-12-17 22:20:13 +0100

    adjusted Pipeline to store oerinfo results in seperate db

--- oer_scrapy/oer_scrapy/pipelines.py
+++ oer_scrapy/oer_scrapy/pipelines.py
@@ -32,48 +32,41 @@ class TagPipeline(object):
 class NormLicensePipeline(object):
     def process_item(self, item, spider):
         print("in Pipeline item license is", item["license"])
-        if any(x in item["license"].lower() for x in ["0", "public domain"]):
-            item["license"] = "CC 0"
-            return item
-        elif all(x in item['license'].lower() for x in ["sa", "by"]) and not "nc" in item["license"].lower():
-            item["license"] = "CC BY-SA"
-            return item
-        elif any(x in item['license'].lower() for x in ["sa", "nd", "nc"]) == False:
-            item["license"] = "CC BY"
-            return item
-        elif "sa" and "nc" in item["license"].lower():
-            item["license"] = "CC BY-SA-NC"
-            return item
-        elif "nd" and "nc" in item["license"].lower():
-            item["license"] = "CC BY-NC-ND"
-            return item
-        elif "nd" and not "nc" in item["license"].lower():
-            item["license"] = "CC BY-ND"
-            return item
-        elif "nc" in item['license'].lower() and (any(x in item['license'].lower() for x in ["nd", "sa"]) == False):
-            item["license"] = "CC BY-NC"
-            return item
-        else:
-            raise DropItem("Missing or unknown license in %s" % item)
+        if item['license']:
+            if any(x in item["license"].lower() for x in ["0", "public domain"]) and not "." in item["license"].lower():
+                item["license"] = "CC 0"
+                return item
+            elif all(x in item['license'].lower() for x in ["sa", "by"]) and not "nc" in item["license"].lower():
+                item["license"] = "CC BY-SA"
+                return item
+            elif any(x in item['license'].lower() for x in ["sa", "nd", "nc"]) == False:
+                item["license"] = "CC BY"
+                return item
+            elif all(x in item["license"].lower() for x in ["by","sa","nc"]) == True:
+                item["license"] = "CC BY-SA-NC"
+                return item
+            elif all(x in item["license"].lower() for x in ["by", "nc", "nd"]) == True:
+                item["license"] = "CC BY-NC-ND"
+                return item
+            elif "nd" and not "nc" in item["license"].lower():
+                item["license"] = "CC BY-ND"
+                return item
+            elif "nc" in item['license'].lower() and (any(x in item['license'].lower() for x in ["nd", "sa"]) == False):
+                item["license"] = "CC BY-NC"
+                return item
+            else:
+                raise DropItem("Missing or unknown license in %s" % item)
 
-class JsonWriterPipeline(object):
-
-    def open_spider(self, spider):
-        self.file = open('items.jl', 'w')
-
-    def close_spider(self, spider):
-        self.file.close()
-
-    def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
-        self.file.write(line)
-        return item
 
 class MySqlPipeline(object):
 
     def __init__(self):
         self.create_connection()
-        self.create_table()
+
+    def open_spider(self, spider):
+        spider_name = spider.name
+        print(spider_name)
+        self.create_table(spider_name)
 
     def create_connection(self):
         self.conn = mysql.connector.connect(
@@ -84,74 +77,144 @@ class MySqlPipeline(object):
         )
         self.curr = self.conn.cursor()
 
-    def create_table(self):
-        self.curr.execute("""DROP TABLE IF EXISTS oerhoernchen_tb""")
-        self.curr.execute("""create table oerhoernchen_tb(
-            name text,
-            about text,
-            author text,
-            publisher text,
-            inLanguage text,
-            accessibilityAPI text,
-            accessibilityControl text,
-            accessibilityFeature text,
-            accessibilityHazard text,
-            license text,
-            timeRequired text,
-            educationalRole text,
-            alignmentType text,
-            educationalFramework text,
-            targetDescription text,
-            targetName text,
-            targetURL text,
-            educationalUse text,
-            typicalAgeRange text,
-            interactivityType text,
-            learningResourceType text,
-            date_published text,
-            url text,
-            thumbnail text,
-            tags text,
-            project text,
-            source text,
-            spider text,
-            date_scraped datetime
-        )""")
+    def create_table(self, spider_name):
+        if spider_name == 'hoou_spider':
+            self.curr.execute("""DROP TABLE IF EXISTS hoou_tb""")
+            self.curr.execute("""create table hoou_tb(
+                name text,
+                about text,
+                author text,
+                publisher text,
+                inLanguage text,
+                accessibilityAPI text,
+                accessibilityControl text,
+                accessibilityFeature text,
+                accessibilityHazard text,
+                license text,
+                timeRequired text,
+                educationalRole text,
+                alignmentType text,
+                educationalFramework text,
+                targetDescription text,
+                targetName text,
+                targetURL text,
+                educationalUse text,
+                typicalAgeRange text,
+                interactivityType text,
+                learningResourceType text,
+                date_published text,
+                url text,
+                thumbnail text,
+                tags text,
+                project text,
+                source text,
+                spider text,
+                date_scraped datetime
+            )""")
+        elif spider_name == 'oerinfo_spider':
+            self.curr.execute("""DROP TABLE IF EXISTS oerinfo_tb""")
+            self.curr.execute("""create table oerinfo_tb(
+                name text,
+                about text,
+                author text,
+                publisher text,
+                inLanguage text,
+                accessibilityAPI text,
+                accessibilityControl text,
+                accessibilityFeature text,
+                accessibilityHazard text,
+                license text,
+                timeRequired text,
+                educationalRole text,
+                alignmentType text,
+                educationalFramework text,
+                targetDescription text,
+                targetName text,
+                targetURL text,
+                educationalUse text,
+                typicalAgeRange text,
+                interactivityType text,
+                learningResourceType text,
+                date_published text,
+                url text,
+                thumbnail text,
+                tags text,
+                project text,
+                source text,
+                spider text,
+                date_scraped datetime
+            )""")
 
     def process_item(self, item, spider):
-        self.store_db(item)
+        self.store_db(item, spider)
         return item
 
-    def store_db(self, item):
-        self.curr.execute("""insert into oerhoernchen_tb values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", (
-            item['name'],
-            item['about'],
-            item['author'],
-            item['publisher'],
-            item['inLanguage'],
-            item['accessibilityAPI'],
-            item['accessibilityControl'],
-            item['accessibilityFeature'],
-            item['accessibilityHazard'],
-            item['license'],
-            item['timeRequired'],
-            item['educationalRole'],
-            item['alignmentType'],
-            item['educationalFramework'],
-            item['targetDescription'],
-            item['targetName'],
-            item['targetURL'],
-            item['educationalUse'],
-            item['typicalAgeRange'],
-            item['interactivityType'],
-            item['learningResourceType'],
-            item['date_published'],
-            item['url'],
-            item['thumbnail'],
-            item['tags'],
-            item['project'],
-            item['source'],
-            item['spider'],
-            item['date_scraped']
-        ))
-        self.conn.commit()
+    def store_db(self, item, spider):
+        spider_name = spider.name
+        if spider_name == 'hoou_spider':
+            self.curr.execute("""insert into hoou_tb values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", (
+                item['name'],
+                item['about'],
+                item['author'],
+                item['publisher'],
+                item['inLanguage'],
+                item['accessibilityAPI'],
+                item['accessibilityControl'],
+                item['accessibilityFeature'],
+                item['accessibilityHazard'],
+                item['license'],
+                item['timeRequired'],
+                item['educationalRole'],
+                item['alignmentType'],
+                item['educationalFramework'],
+                item['targetDescription'],
+                item['targetName'],
+                item['targetURL'],
+                item['educationalUse'],
+                item['typicalAgeRange'],
+                item['interactivityType'],
+                item['learningResourceType'],
+                item['date_published'],
+                item['url'],
+                item['thumbnail'],
+                item['tags'],
+                item['project'],
+                item['source'],
+                item['spider'],
+                item['date_scraped']
+            ))
+            self.conn.commit()
+        elif spider_name == 'oerinfo_spider':
+            self.curr.execute("""insert into oerinfo_tb values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", (
+                item['name'],
+                item['about'],
+                item['author'],
+                item['publisher'],
+                item['inLanguage'],
+                item['accessibilityAPI'],
+                item['accessibilityControl'],
+                item['accessibilityFeature'],
+                item['accessibilityHazard'],
+                item['license'],
+                item['timeRequired'],
+                item['educationalRole'],
+                item['alignmentType'],
+                item['educationalFramework'],
+                item['targetDescription'],
+                item['targetName'],
+                item['targetURL'],
+                item['educationalUse'],
+                item['typicalAgeRange'],
+                item['interactivityType'],
+                item['learningResourceType'],
+                item['date_published'],
+                item['url'],
+                item['thumbnail'],
+                item['tags'],
+                item['project'],
+                item['source'],
+                item['spider'],
+                item['date_scraped']
+            ))
+            self.conn.commit()
+
